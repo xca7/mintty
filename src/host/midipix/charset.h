dc278e4b4ad9b0be1547d6a4309f209ea9c33e79
midipix <writeonce@midipix.org>
2017-10-31 23:51:45 -0400

    host-specific std.h and charset.h: initial commit.

---
+++ src/host/midipix/charset.h
@@ -0,0 +1,12 @@
+#ifndef _HOST_CHARSET_H_
+#define _HOST_CHARSET_H_
+
+#ifndef CHARSET_H
+#define CHARSET_H
+#endif
+
+#ifndef HAS_LOCALES
+#define HAS_LOCALES 1
+#endif
+
+#endif
---
+++ src/host/midipix/std.h
@@ -0,0 +1,25 @@
+#ifndef _HOST_STD_H_
+#define _HOST_STD_H_
+
+/*****************************************************************/
+/* mintty was (and still is) written for 16-bit wide characters. */
+/* for as long as this remains the case, providing a surrogate   */
+/* std.h for portable targets seems to be the least painful      */
+/* path to a successful port.                                    */
+/*****************************************************************/
+
+#include <features.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <wchar.h>
+
+size_t     wcslen_host (const uint16_t *);
+uint16_t * wcsstr_host (const uint16_t *, const wchar_t *);
+
+#define wcslen	wcslen_host
+#define wcsstr	wcsstr_host
+
+#include "charset.h"
+#include "../../std.h"
+
+#endif

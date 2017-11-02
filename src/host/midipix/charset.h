d5e451bdfe75aa2e719959295fb3ae045e3cf426
midipix <writeonce@midipix.org>
2017-11-01 22:26:15 -0400

    porting: host-specific headers: added defs needed for compilation of charset.c.

--- src/host/midipix/charset.h
+++ src/host/midipix/charset.h
@@ -5,4 +5,31 @@
 #define HAS_LOCALES 1
 #endif
 
+#include <stdint.h>
+#include <stddef.h>
+
+#include <windef.h>
+#include <wingdi.h>
+
+int cs_strconv_utf16_to_utf8(
+	uint32_t		cp,
+	uint32_t		flags,
+	const uint16_t *	wch,
+	int			wchlen,
+	char *			ch,
+	int			chlen,
+	const char *		defchar,
+	int *			defcharused);
+
+int cs_strconv_utf8_to_utf16(
+	uint32_t		cp,
+	uint32_t		flags,
+	const char *		ch,
+	int			chlen,
+	uint16_t *		wch,
+	int			wchlen);
+
+#define WideCharToMultiByte	cs_strconv_utf16_to_utf8
+#define MultiByteToWideChar	cs_strconv_utf8_to_utf16
+
 #endif
--- src/host/midipix/std.h
+++ src/host/midipix/std.h
@@ -1,6 +1,8 @@
 #ifndef _HOST_STD_H_
 #define _HOST_STD_H_
 
+#define  WINAPI_UTF16_DEFAULT
+
 /*****************************************************************/
 /* mintty was (and still is) written for 16-bit wide characters. */
 /* for as long as this remains the case, providing a surrogate   */
@@ -11,14 +13,25 @@
 #include <features.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <wchar.h>
+#include <sys/ioctl.h>
 
-size_t     wcslen_host (const uint16_t *);
-uint16_t * wcsstr_host (const uint16_t *, const wchar_t *);
 
-#define wcslen	wcslen_host
-#define wcsstr	wcsstr_host
+size_t     wcslen_host        (const uint16_t *);
+uint16_t * wcsstr_host        (const uint16_t *, const wchar_t *);
+uint16_t * wcsdup_host        (const uint16_t * string);
+uint16_t * wcscpy_host        (uint16_t *, const uint16_t *);
+size_t     mbstowcs_host      (uint16_t *, const char *, size_t n);
+size_t     mbrtowc_host       (uint16_t *, const char *, size_t, mbstate_t *);
+
+#define    wcslen              wcslen_host
+#define    wcsstr              wcsstr_host
+#define    wcsdup              wcsdup_host
+#define    wcscpy              wcscpy_host
+#define    mbstowcs            mbstowcs_host
+#define    mbrtowc             mbrtowc_host
 
 #include "charset.h"
 #include "literals.h"

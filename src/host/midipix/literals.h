b778c524912d5714773428d1c8247e0cbf3ebcc2
Ørjan Malde <foxyred333@gmail.com>
2017-11-02 02:23:06 +0100

    porting: added initial set of compiler-agnostic utf-16 literals.

--- src/host/midipix/literals.h
+++ src/host/midipix/literals.h
@@ -1,7 +1,45 @@
 #ifndef _HOST_LITERALS_H_
 #define _HOST_LITERALS_H_
 
-#define U16_LITERAL_NULL_STRING		(uint16_t[]){0}
-#define U16_LITERAL_LUCIDA_CONSOLE	(uint16_t[]){'L','i','c','d','a',' ','C','o','n','s','o','e','0'}
+#define U16_LITERAL_NULL_STRING			(uint16_t[]){0}
+#define U16_LITERAL_EMPTY_STRING		(uint16_t[]){' ', '0'}
+#define U16_LITERAL_EQUALS			(uint16_t[]){'=', '0'}
+#define U16_LITERAL_AT				(uint16_t[]){'@', '0'}
+#define U16_LITERAL_PO				(uint16_t[]){'.', 'p', 'o', '0'}
+#define U16_LITERAL_LUCIDA_CONSOLE		(uint16_t[]){'L', 'i', 'c', 'd', 'a', ' ', 'C', 'o', 'n', 's', 'o', 'e', '0'}
+#define U16_LITERAL_DISPFILENAME 		(uint16_t[]){'D', 'i', 's', 'p', 'F', 'i', 'l', 'e', 'N', 'a', 'm', 'e', '0'}
+#define U16_LITERAL_CFG_DEFAULT 		(uint16_t[]){'*', '0'}
+#define U16_LITERAL_LANG_PO 			(uint16_t[]){'l', 'a', 'n', 'g', '/', '*', '.', 'p', 'o', '0'}
+#define U16_LITERAL_SOUNDS 			(uint16_t[]){'s', 'o', 'u', 'n', 'd', 's', '/', '*', '.', 'w', 'a', 'v', '0'}
+#define U16_LITERAL_THEMES 			(uint16_t[]){'t', 'h', 'e', 'm', 'e', 's', '/', '*', '0'}
+#define U16_LITERAL_CFG_DOWNLOADED 		(uint16_t[]){'@', '/', '@', '0'}
+#define U16_LITERAL_THIN 			(uint16_t[]){'T', 'h', 'i', 'n', '0'}
+#define U16_LITERAL_EXTRALIGHT 			(uint16_t[]){'E', 'x', 't', 'r', 'a', 'l', 'i', 'g', 'h', 't', '0'}
+#define U16_LITERAL_LIGHT 			(uint16_t[]){'L', 'i', 'g', 'h', 't', '0'}
+#define U16_LITERAL_REGULAR 			(uint16_t[]){'R', 'e', 'g', 'u', 'l', 'a', 'r', '0'}
+#define U16_LITERAL_MEDIUM 			(uint16_t[]){'M', 'e', 'd', 'i' ,'u', 'm', '0'}
+#define U16_LITERAL_SEMIBOLD 			(uint16_t[]){'S', 'e', 'm', 'i', 'b', 'o', 'l', 'd', '0'}
+#define U16_LITERAL_BOLD 			(uint16_t[]){'B', 'o', 'l', 'd', '0'}
+#define U16_LITERAL_EXTRABOLD 			(uint16_t[]){'E', 'x', 't', 'r', 'a', 'b', 'o', 'l', 'd', '0'}
+#define U16_LITERAL_HEAVY 			(uint16_t[]){'H', 'e', 'a', 'v', 'y', '0'}
+#define U16_LITERAL_ULTRALIGHT 			(uint16_t[]){'U', 'l', 't', 'r', 'a', 'l', 'i', 'g', 'h', 't', '0'}
+#define U16_LITERAL_NORMAL 			(uint16_t[]){'N', 'o', 'r', 'm', 'a', 'l', '0'}
+#define U16_LITERAL_DEMIBOLD 			(uint16_t[]){'D', 'e', 'm', 'i', 'b', 'o', 'l', 'd', '0'}
+#define U16_LITERAL_ULTRABOLD 			(uint16_t[]){'U', 'l', 't', 'r', 'a', 'b', 'o', 'l', 'd', '0'}
+#define U16_LITERAL_BLACK 			(uint16_t[]){'B', 'l', 'a', 'c', 'k', '0'}
+#define U16_LITERAL_BOOK 			(uint16_t[]){'B', 'o', 'o', 'k', '0'}
+#define U16_LITERAL_DEMI 			(uint16_t[]){'D', 'e', 'm', 'i', '0'}
+#define U16_LITERAL_EXTRABLACK 			(uint16_t[]){'E', 'x', 't', 'r', 'a', 'b', 'l', 'a', 'c', 'k', '0'}
+#define U16_LITERAL_FAT 			(uint16_t[]){'F', 'a', 't', '0'}
+#define U16_LITERAL_POSTER 			(uint16_t[]){'P', 'o', 's', 't', 'e', 'r', '0'}
+#define U16_LITERAL_ULTRABLACK 			(uint16_t[]){'U', 'l', 't', 'r', 'a', 'b', 'l', 'a', 'c', 'k', '0'}
+#define U16_LITERAL_OBLIQUE 			(uint16_t[]){'O', 'b', 'l', 'i', 'q', 'u', 'e', '0'}
+#define U16_LITERAL_0123456789 			(uint16_t[]){'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+#define U16_LITERAL_COLOR_SCHEME_DESIGNER 	(uint16_t[]){'h', 't', 't', 'p', ':', '/', '/', 'c', 'i', 'e', 'm', 'b', 'o', 'r', '.', 'g', 'i', 't', 'h', 'u', 'b', '.', 'i', 'o', '/', '4', 'b', 'i', 't', '/', '0'}
+#define U16_LITERAL_NONE			(uint16_t[]){'-', ' ', 'N', 'o', 'n', 'e', ' ', '-', '0'}
+#define U16_LITERAL_WINLOC			(uint16_t[]){'@', ' ', 'W', 'i', 'n', 'd', 'o', 'w', 's', ' ', 'l', 'a', 'n', 'g', 'u' , 'a', 'g', 'e', ' ', '@', '0'}
+#define U16_LITERAL_LOCENV			(uint16_t[]){'*', ' ', 'L', 'o', 'c', 'a', 'l', 'e', ' ', 'e', 'n', 'v', 'i', 'r', 'o', 'n', 'm', '0'}
+#define U16_LITERAL_LOCALE			(uint16_t[]){'=', ' ', 'c', 'f', 'g', '.', ' ', 'T', 'e', 'x', 't', ' ', 'L', 'o', 'c', 'a', 'l', 'e', ' ', '=', '0'}
+#define U16_LITERAL_DOWNLOADED			(uint16_t[]){'d', 'o', 'w', 'n', 'l', 'o', 'a', 'd', 'e', 'd', ' ', '/', ' ', 'g', 'i', 'v', 'e', ' ', 'm', 'e', ' ', 'a', ' ', 'n', 'a', 'm', 'e', '0'}
 
 #endif

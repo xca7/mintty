e19dd3818d55385e14358205d0820d6dc92e5374
midipix <writeonce@midipix.org>
2017-11-12 15:50:58 -0500

    porting: defined framework-specific path conversion interfaces.

--- src/host/midipix/charset.h
+++ src/host/midipix/charset.h
@@ -11,6 +11,10 @@
 #include <windef.h>
 #include <wingdi.h>
 
+char *     cs_pathconv_posix_utf8_to_w32cmd_utf8(const char *);
+uint16_t * cs_pathconv_posix_utf8_to_w32cmd_utf16(const char *);
+char *     cs_pathconv_w32cmd_utf16_to_posix_utf8(const uint16_t *);
+
 int cs_strconv_utf16_to_utf8(
 	uint32_t		cp,
 	uint32_t		flags,
@@ -36,6 +40,10 @@ int cs_print_font_info(
 	const uint16_t *	st_utf16,
 	const uint16_t *	face_utf16,
 	const uint16_t *	style_utf16);
+
+#define path_posix_to_win_a	cs_pathconv_posix_utf8_to_w32cmd_utf8
+#define path_posix_to_win_w	cs_pathconv_posix_utf8_to_w32cmd_utf16
+#define path_win_w_to_posix	cs_pathconv_w32cmd_utf16_to_posix_utf8
 
 #define WideCharToMultiByte	cs_strconv_utf16_to_utf8
 #define MultiByteToWideChar	cs_strconv_utf8_to_utf16
